0eaccb10bf64db2b067556126188bb2ab3c09ce4
ixan droid <ihsan.implementasi@gmail.com>
2013-05-02 21:27:24 +0700

    perubahan database nambah view alokasi add progress fitur btn next action
    add record menu
    
    svworking	Alokasi / Pengerjaan	service	2	NULL	/sv-Working/index

---
+++ Seemok/Controllers/Service/WorkingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Seemok.Controllers.Service
+{
+    public class WorkingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
